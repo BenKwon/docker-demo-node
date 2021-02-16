791395fb06995a6dcaed69e3bdf73aa5dd36bb3e
BenKwon <kwon5891@gmail.com>
2021-02-16 16:33:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! git hub webhook test with jenkins')
+  res.send('jenkins webhook test push')
 })
 
 app.listen(port, () => {
